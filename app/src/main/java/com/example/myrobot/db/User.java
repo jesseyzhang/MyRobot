62a3d661bbd90d8ce50d840d33ef2d0655312ff1
jessey <jesseyzhang@126.com>
2017-10-14 10:52:32 +0800

    增加了登陆注册页面和数据库的配置

---
+++ app/src/main/java/com/example/myrobot/db/User.java
@@ -0,0 +1,38 @@
+package com.example.myrobot.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by DELL 15 on 2017/10/14.
+ */
+
+public class User extends DataSupport {
+
+    private int id;
+    private String userName;
+    private String passWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
